67c2acc4f2a15051557c90c0a4d7f0ba0a7a3002
Игорь Карпук <igorek.karpuk@gmail.com>
2018-12-06 16:10:59 +0300

    fix styles for header mobile version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,32 +13,6 @@ const Header = (props) => {
     genres,
     bg: { backgroundImage: `url(http://image.tmdb.org/t/p/w1280${films[0].backdrop_path})` },
   };
-  // data = {
-  //   genres: [
-  // {
-  //   id: 18,
-  //   name: 'Drama',
-  // },
-  // {
-  //   id: 10751,
-  //   name: 'Family',
-  // },
-  // {
-  //   id: 14,
-  //   name: 'Fantasy',
-  // },
-  //   ],
-  //   films: [
-  //     {
-  //       filmGenres: [18, 10751, 14],
-  //       filmName: 'Fantastic Beasts: The Crimes of Grindelwald default',
-  //       filmDescription: 'Gellert Grindelwald has escaped imprisonment and has begun gathering.',
-  //       filmRate: 7.1,
-  //       filmId: 338952,
-  //     },
-  //   ],
-  //   bg: { backgroundImage: 'url(http://image.tmdb.org/t/p/w1280/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg)' },
-  // };
   return (
     <header id="app_header" className={style.app_header} style={data.bg}>
       <HeaderTopContainer />

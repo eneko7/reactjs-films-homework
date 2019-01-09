002b24e08bd9f63de00a221ae0dc49816e05aa02
Игорь Карпук <igorek.karpuk@gmail.com>
2019-01-09 12:30:51 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
-import MovieDetailsPage from '../../pages/MovieDetailsPage';
+import MoviesGrid from '../MoviesGrid';
 import PageNotFound from '../../pages/PageNotFound';
 import Footer from '../Footer';
 import Header from '../Header';
@@ -10,11 +10,11 @@ const App = () => (
   <div className="main_container">
     <Header />
     <Switch>
-      <Route exact path="/" component={MovieDetailsPage} />
-      <Route exact path="/films" component={MovieDetailsPage} />
-      <Route exact path="/film" component={MovieDetailsPage} />
-      <Route exact path="/search" component={MovieDetailsPage} />
-      <Route exact path="/genres" component={MovieDetailsPage} />
+      <Route exact path="/" component={MoviesGrid} />
+      <Route exact path="/films" component={MoviesGrid} />
+      <Route exact path="/film" component={MoviesGrid} />
+      <Route exact path="/search" component={MoviesGrid} />
+      <Route exact path="/genres" component={MoviesGrid} />
       <Route path="*" component={PageNotFound} />
     </Switch>
     <Footer />
